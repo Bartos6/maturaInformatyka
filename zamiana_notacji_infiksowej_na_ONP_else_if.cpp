49b2da1c54fefa30ace812a2f998c3ddb4f2dbdb
Bartos6 <130593832+Bartos6@users.noreply.github.com>
2025-09-22 16:23:09 +0200

    Create zamiana_notacji_infiksowej_na_ONP_else_if.cpp

---
+++ zamiana_notacji_infiksowej_na_ONP_else_if.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main()
+{
+	string w; //dzialanie (bez spacji) na cyfrach
+	cin >> w;
+	stack <char> stos;
+	stos.push('#');
+
+	for (int i = 0; i < w.size(); i++) {
+		//sprawdzenie czy liczba
+		if (w[i] >= '0' && w[i] <= '9') {
+			cout << w[i];
+		}
+
+		//sprawdzenie operatora + -
+		if (w[i] == '+' || w[i] == '-') {
+			while (stos.top() != '#' && stos.top() != '(') {
+				cout << stos.top();
+				stos.pop();
+			}
+			stos.push(w[i]);
+		}
+
+		//sprawdzenie operatora * /
+		else if (w[i] == '*' || w[i] == '/') {
+			while (stos.top() != '#' && stos.top() != '(' && stos.top() != '+' && stos.top() != '-') {
+				cout << stos.top();
+				stos.pop();
+			}
+			stos.push(w[i]);
+		}
+
+		//sprawdzenie operatora ^
+		else if (w[i] == '^') {
+			while (stos.top() == '^') {
+				cout << stos.top();
+				stos.pop();
+			}
+			stos.push(w[i]);
+		}
+
+		//nawias otwierajacy 
+		else if (w[i] == '(') {
+			stos.push(w[i]);
+		}
+
+		//nawias zamykajacy
+		else if (w[i] == ')') {
+			while (stos.top() != '(') {
+				cout << stos.top();
+				stos.pop();
+			}
+			stos.pop();
+		}
+	}
+
+	while (!stos.empty()) {
+		cout << stos.top();
+		stos.pop();
+	}
+
+}
