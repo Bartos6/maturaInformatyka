30e7228e2bfff9ed617e13f7cb7c5837388889df
Bartos6 <130593832+Bartos6@users.noreply.github.com>
2025-09-25 14:05:12 +0200

    Create zmiana_miedzy_systemami_liczbowymi.cpp

---
+++ zmiana_miedzy_systemami_liczbowymi.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+
+
+
+void zmianaSystemu(int liczba, int ps) {
+	int r, tab[100], i = 0;
+
+	while (liczba != 0) {
+		r = liczba % ps;
+		liczba = liczba / ps;
+		tab[i] = r;
+		i++;
+	}
+
+	for (int j = i - 1; j >= 0; j--) {
+		cout << tab[j];
+	}
+}
+
+string zmianaSystemuNapis(int liczba, int ps) {
+	int r, tab[100], i = 0;
+	string wynik = "";
+	char znak;
+	while (liczba != 0) {
+		tab[i++] = liczba % ps;
+		liczba /= ps;
+	}
+
+	for (int j = i - 1; j >= 0; j--) {
+		znak = tab[j] + 48;   //	Można sprawdzić odpowiednik cyfr w ASCII cout << "0: " << int('0') << endl; UWAGA! Nie zadziała dla systemu większego niż 9
+
+		wynik += znak;
+	}	
+
+	return wynik;
+
+}
+
+void zmianaSystemuWynikDoPliku(int liczba, int ps) {
+	ofstream wynikF("wynikFunkcja.txt");
+
+	int r, tab[100], i = 0;
+	string wynik = "";
+	char znak;
+	while (liczba != 0) {
+		tab[i++] = liczba % ps;
+		liczba /= ps;
+	}
+
+	for (int j = i - 1; j >= 0; j--) {
+		wynikF << tab[j];
+	}
+
+}
+
+
+int main()
+{
+	ifstream plik("dane.txt");
+	ofstream wynik("wynik.txt");
+
+	int liczba, podstawaSystemu;
+	plik >> liczba >> podstawaSystemu;
+	//zapis do pliku
+	wynik << zmianaSystemuNapis(liczba, podstawaSystemu);
+	zmianaSystemuWynikDoPliku(liczba, podstawaSystemu);
+	//wypisaie na ekran
+	zmianaSystemu(liczba, podstawaSystemu);
+	
+
+	plik.close();
+	wynik.close();
+
+
+	return 0;
+}
