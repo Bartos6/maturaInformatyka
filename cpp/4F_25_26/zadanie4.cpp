30cd5f4af088e8ff5436c4ad8125d4990c5b5a81
Bartos6 <130593832+Bartos6@users.noreply.github.com>
2025-12-18 11:01:08 +0100

    Add files via upload

---
+++ cpp/4F_25_26/zadanie4.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    ifstream dane("zadanie4fKRK.txt");
+    ofstream wynik("wynik.txt");
+
+    string napis;
+    
+    
+    while (getline(dane, napis)) {
+        int licznik = 0;
+
+        for (int i = 0; i < napis.size(); i++) {
+
+            if (napis[i] == 'a') {
+                licznik++;
+            }
+        }
+        cout << licznik << " ";
+    }
+
+}
+
